fe75d4e9fcd0580324b1ab280a9892b4c6be0e62
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-10-16 21:24:52 -0400

    Api news, improved

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,7 @@
 
 <script>
 
-import NewsApiService from './services/NewsApiService'
-
-const API_KEY = '148911f6e11c4568be8c2fb54bc7a938'
-const service = new NewsApiService(API_KEY)
-
-const DEFAULT_QUERY = 'javascript'
-
 export default {
-  name: 'App',
-
-  data: function() {
-    return {
-      articles: []
-    }
-  },
-
-  created: function() {
-    service.getNews(DEFAULT_QUERY)
-      .then(this.setArticles)
-  },
-
-  methods: {
-    setArticles: function(articles) {
-      this.articles = articles
-    }
-  }
+  name: 'App'
 }
 </script>
--- src/components/FeaturedPost.vue
+++ src/components/FeaturedPost.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="card flex-md-row mb-4 shadow-sm h-md-250">
     <div class="featured-post-content card-body d-flex flex-column align-items-start">
-      <strong class="d-inline-block mb-2 text-primary">{{ article.author }}</strong>
+      <a target="_blank" :href="hrefSource"><strong class="d-inline-block mb-2 text-primary">{{ article.source.name }}</strong></a>
       <h3 class="mb-0">
-        <router-link class="text-dark" :to="{ name: 'PostDetails', params: { id: article.key } }">{{ article.title }}</router-link>
+        <router-link class="text-dark" :to="{ name: 'PostDetails', params: { id: id } }">{{ article.title }}</router-link>
       </h3>
       <div class="mb-1 text-muted">{{ publishedAt }}</div>
     </div>
@@ -13,10 +13,12 @@
 
 <script>
 import moment from 'moment'
+import {getUrlDomain} from '../helpers'
 
 export default {
   name: 'FeaturedPost',
   props: {
+    id: String,
     article: Object
   },
   computed: {
@@ -25,6 +27,9 @@ export default {
     },
     publishedAt: function() {
       return moment(this.article.publishedAt).fromNow()
+    },
+    hrefSource: function() {
+      return getUrlDomain(this.article.url)
     }
   }
   
